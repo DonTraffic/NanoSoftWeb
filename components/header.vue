39e78ed35188e4dec2b2b3594b0c1a4e8fe5c4c3
DonTraffic <dogerfantom6@gmail.com>
2025-09-24 00:23:20 +0300

    Старт проекта (раб.обл. + шапка)

---
+++ components/header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header class="header">
+        <div class="header__menu">
+            <button class="header__menu-btn">О компании</button>
+            <button class="header__menu-btn">Услуги</button>
+            <button class="header__menu-btn">Кейсы</button>
+            <button class="header__menu-btn">Контакты</button>
+        </div>
+
+        <div class="header-container">
+            <div class="header__subtitle">разработка / внедрение / поддержка</div>
+
+            <!-- <svg class="header__logo">
+                <use xlink:href="@/assets/sprites/sprite.svg#NSLogo"></use>
+            </svg> -->
+
+            <img class="header__logo" src="@/assets/sprites/NSLogo.svg" alt="Логотип">
+
+            <div class="header__title">
+                <div class="header__title-container">
+                    <span class="header__title-text">Экспертные решения</span>
+                    <div class="header__title-transfer">
+                        <span class="header__title-transfer-item">1С</span>
+                        <span class="header__title-transfer-item">CRM</span>
+                        <span class="header__title-transfer-item">Telegram-боты</span>
+                    </div>
+                </div>
+
+                <span class="header__title-text">для вашего бизнеса</span>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+    @use "@/assets/styles/main.scss";
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <Header />
+
+    <h2>new page</h2>
+</template>
+
+<script setup>
+
+</script>
